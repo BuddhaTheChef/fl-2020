492629580dd2aac291de941f13e856bd1dc4976a
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-10-08 12:24:23 -0700

    adding in routes

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link,Route, Switch } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Work from './components/Work/Work';
+import Contact from './components/Contact/Contact';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/work">Work</Link>
+        <Link to="/Contact">Contact</Link>
+      </div>
+        <h1>Freelance Site</h1>
+        <Switch>
+          <Route component={Home} exact path="/" />
+          <Route component={About} exact path="/about" />
+          <Route component={Work} exact path="/work" />
+          <Route component={Contact} exact path="/contact" />
+        </Switch>
+    </div>
+  );
+}
+
+export default App;
