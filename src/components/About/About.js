02b60aa048bb5e396f02f7067df44de0b9ae3155
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-10-13 14:08:08 -0700

    adding in styles

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,13 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                <h1 className="title">About</h1>
+            <div className="main-about-div">
+                <div className="header-about-div-l"><p style={{padding: '95px', fontSize: '40px'}}>This is a small description. Or a little bit ab out your career.</p></div>
+                <div className="header-about-div-r"><h1 className="title">About</h1></div>
+            </div>
+            <div className="lower-about-div">
+                    Other content
+            </div>
             </div>
         )
     }
