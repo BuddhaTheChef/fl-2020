e571a7b4c3c8aa4c844a3b35ac7c8fba4b30c619
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-10-23 14:33:24 -0700

    adding in styles for home and about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,12 +8,25 @@ export default class About extends Component {
                 <div className="header-about-div-l"><p style={{padding: '95px', fontSize: '40px'}}>This is a small description. Or a little bit ab out your career.</p></div>
                 <div className="header-about-div-r"><h1 className="title">About</h1></div>
             </div>
-            <div className="lower-about-div">
+
+            <div className="main-about-div">
+            <div className="header-about-div-r">
                     <h1 style={{color:'black', display: 'inline'}}>Other content</h1>
             </div>
-            <div className="lower-about-div">
+            <div className="header-about-div-l">
                     <h1 style={{color:'black', display: 'inline'}}>Other content</h1>
             </div>
+            </div>
+
+            <div className="main-about-div">
+            <div className="header-about-div-l">
+                    <h1 style={{color:'black', display: 'inline'}}>Other content</h1>
+            </div>
+            <div className="header-about-div-r">
+                    <h1 style={{color:'black', display: 'inline'}}>Other content</h1>
+            </div>
+            </div>
+
             </div>
         )
     }
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -8,7 +8,9 @@ export default class Work extends Component {
                 <div className="header-work-div"><p style={{padding: '95px', fontSize: '40px'}}>Work</p></div>
             </div>
             <div className="lower-work-div">
-                <h3>Content here</h3>
+                <div className="work-div-subcontent"><h4>Work 1</h4></div>
+                <div className="work-div-subcontent"><h4>Work 2</h4></div>
+                <div className="work-div-subcontent"><h4>Work 3</h4></div>
             </div>
             </div>
         )
