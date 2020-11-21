646494c32f355118381547c0bf4809030c85b50b
Aj Wietecha II <ajwietechaii@yahoo.com>
2020-11-21 14:21:21 -0800

    modifying footer

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -49,7 +49,43 @@ export default class Home extends Component {
         </div>
         </div>
         <div style={{backgroundColor: 'rgb(66,66,66)', height: '300px', padding:'60px'}}>
-               Future footer
+               <div className="main-footer-div">
+                 <div className="footer-sub-div-left">
+                 <div className="sub-footer-div-1">
+                 <h3>Connect</h3>
+                 <h3>Contribute on GitHub</h3>
+                 <h3>Support on Patreon</h3>
+                 <h3>Join the Discord server</h3>
+                 <h3>Follow us on Twitch</h3>
+                 <h3>Find out more</h3>
+                 <h3>Contact</h3>
+                 </div>
+                 <div className="sub-footer-div-2">
+                 <h3>Get Help</h3>
+                 <p>Order Status</p>
+                 <p>Shipping and Delivery</p>
+                 <p>Returns</p>
+                 <p>Payment Options</p>
+                 <p>Gift Cards</p>
+                 <p>Contact Support</p>
+                 </div>
+                 <div className="sub-footer-div-3">
+                   <h3>About Me</h3>
+                   <p>News</p>
+                   <p>Careers</p>
+                   <p>Blog</p>
+                   <p>Contct</p>
+                   <p>Team</p>
+                   <p>Future Plans</p>
+                  </div>
+                  </div>
+                 <div className="sub-footer-div-4">
+                   <h4>Twitter</h4>
+                   <h4>Instagram</h4>
+                   <h4>Facebook</h4>
+                   <h4>Twitch</h4>
+                  </div>
+               </div>
             </div>
       </div>
     );
