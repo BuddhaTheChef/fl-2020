155836164d73dec08056afd6e978520d3c4bfbe4
Aj Wietecha II <ajwietechaii@yahoo.com>
2021-01-01 13:46:45 -0800

    added in timeline

--- src/components/Contact/ContactUs.js
+++ src/components/Contact/ContactUs.js
@@ -17,18 +17,92 @@ export default function ContactUs() {
   }
 
   return (
-    <div>
-        <h1>Contact Us</h1>
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
+    // <div>
+    //   <div>
+    //     <h1>Contact Us</h1>
+    //     </div>
+    //     <div>
+    // <form className="contact-form" onSubmit={sendEmail}  style={{display: 'flex',flexDirection: 'column',height: '60vh', width: '70vw'}}>
+    //   <input type="hidden" name="contact_number" />
+    //   <label>Name</label>
+    //   <input type="text" name="user_name" />
+    //   <label>Email</label>
+    //   <input type="email" name="user_email" />
+    //   <label>Message</label>
+    //   <textarea name="message" />
+    //   <input type="submit" value="Send" />
+    // </form>
+    // </div>
+    // </div>
+
+
+<section id="contact">
+  
+  <h1 class="section-header">CONTACT</h1>
+  
+  <div class="contact-wrapper">
+
+    
+    <form class="form-horizontal" onSubmit={sendEmail}>
+       
+      <div class="form-group">
+        <div class="col-sm-12">
+          <input type="text" class="form-control" id="name" placeholder="NAME" name="user_name" value=""/>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="col-sm-12">
+          <input type="email" class="form-control" id="email" placeholder="EMAIL" name="user_email" value=""/>
+        </div>
+      </div>
+
+      <textarea class="form-control" rows="10" placeholder="MESSAGE" name="message"></textarea>
+      
+      <button class="btn btn-primary send-button" id="submit" type="submit" value="Send">
+        <div class="button">
+          <i class="fa fa-paper-plane"></i><span class="send-text">SEND</span>
+        </div>
+      
+      </button>
+      
     </form>
-    </div>
+    
+
+      <div class="direct-contact-container">
+
+        <ul class="contact-list">
+          <li class="list-item"><i class="fa fa-map-marker fa-2x"><span class="contact-text place">Seattle | WA</span></i></li>
+          
+          <li class="list-item"><i class="fa fa-phone fa-2x"><span class="contact-text phone"><a href="+1(123)456-7890" title="Give me a call">(123)456-7890</a></span></i></li>
+          
+          <li class="list-item"><i class="fa fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:#" title="Send me an email">emailme@test.com</a></span></i></li>
+          
+        </ul>
+
+        <hr/>
+        <ul class="social-media-list">
+          <li><a href="#" target="_blank" class="contact-icon">
+            <i class="fa fa-github" aria-hidden="true"></i></a>
+          </li>
+          <li><a href="#" target="_blank" class="contact-icon">
+            <i class="fa fa-twitter" aria-hidden="true"></i></a>
+          </li>
+          <li><a href="#" target="_blank" class="contact-icon">
+            <i class="fa fa-instagram" aria-hidden="true"></i></a>
+          </li>
+          <li><a href="#" target="_blank" class="contact-icon">
+            <i class="fa fa-codepen" aria-hidden="true"></i></a>
+          </li>
+        </ul>
+        <hr/>
+
+      </div>
+    
+  </div>
+  
+</section>
+
   );
+
 }
