2c56804af33676feb1731c72bfb93af47fb00bf0
Aj Wietecha II <ajwietechaii@yahoo.com>
2021-04-19 16:44:27 -0700

    changing router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter basename="/fl-2020">
+    <BrowserRouter basename={window.location.pathname || ''}>
     <App />
     </BrowserRouter>,
   document.getElementById('root')
